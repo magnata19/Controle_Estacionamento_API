ac01fa0c2dbae22bcdad3ed51a23c01792999718
magnata19 <dehpacifico@gmail.com>
2024-06-13 18:28:19 -0300

    criando service e controller para criar um usuario

--- src/main/java/com/dpacifico/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/dpacifico/demo_park_api/service/UsuarioService.java
@@ -1,12 +1,19 @@
 package com.dpacifico.demo_park_api.service;
 
+import com.dpacifico.demo_park_api.entity.Usuario;
 import com.dpacifico.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
