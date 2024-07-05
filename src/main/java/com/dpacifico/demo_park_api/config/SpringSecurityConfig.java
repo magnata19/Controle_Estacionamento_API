3d449b36fb9eae94702594ea5da2a45ca2d9801c
magnata19 <dehpacifico@gmail.com>
2024-07-04 21:53:13 -0300

    iniciando configucao do spring security e jwt

---
+++ src/main/java/com/dpacifico/demo_park_api/config/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.dpacifico.demo_park_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableMethodSecurity
+@EnableWebMvc //necessaria para trabalhar com sistema de segurança
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) //desabilitar stateful
+                .formLogin(form -> form.disable()) //desabilitar o form
+                .httpBasic(basic -> basic.disable())
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(HttpMethod.POST, "api/v1/usuarios").permitAll() //definir permissoes de acesso
+                                .anyRequest().authenticated()
+                ).sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
