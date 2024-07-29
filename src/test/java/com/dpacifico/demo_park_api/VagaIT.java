8ec966d181fb5487ce5d8c98af61ccaad144f9c5
magnata19 <dehpacifico@gmail.com>
2024-07-28 21:19:43 -0300

    documentar metodos de salvar e resgatar uma vaga e criar testes unitarios para os mesmos

---
+++ src/test/java/com/dpacifico/demo_park_api/VagaIT.java
@@ -0,0 +1,33 @@
+package com.dpacifico.demo_park_api;
+
+import com.dpacifico.demo_park_api.web.dto.VagaCreateDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/vagas/vagas-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/vagas/vagas-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class VagaIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void criarVaga_ComDadosValidos_RetornarLocationStatus201() {
+        testClient
+                .post()
+                .uri("/api/v1/vagas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthentication(testClient, "davids@david2.com","123321"))
+                .bodyValue(new VagaCreateDTO("A-05","LIVRE"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().exists(HttpHeaders.LOCATION);
+
+    }
+}
